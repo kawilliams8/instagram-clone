0eff8e23b76fd89de3e74be53f993e65a83d5387
Katie Williams <kawilliams8@gmail.com>
2021-02-21 12:14:28 -0700

    Add Firebase context, wrap app in Provider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { firebase, FieldValue } from "./lib/firebase";
+import FirebaseContext from './context/firebase';
 
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root"));
+ReactDOM.render(
+<FirebaseContext.Provider value={{ firebase, FieldValue }}>
+  <h1>Hello, World</h1>
+</FirebaseContext.Provider>,
+document.getElementById("root")
+);
