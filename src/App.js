e8ae7b90c42f79bb07e44008a816112e203e6970
Katie Williams <kawilliams8@gmail.com>
2021-02-21 13:42:09 -0700

    Add Suspence component with basic fallback text

--- src/App.js
+++ src/App.js
@@ -6,3 +6,11 @@ const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/signup"));
 const Profile = lazy(() => import("./pages/profile"));
 const NotFound = lazy(() => import("./pages/not-found"));
+
+export default function App() {
+  return (
+  <Suspense fallback={<p>...Loading</p>}>
+    <p>Loaded content here.</p>
+  </Suspense>
+  )
+}
