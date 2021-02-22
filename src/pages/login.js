920a96ade5b8faae594f304fb2e207a8c9b65ecc
Katie Williams <kawilliams8@gmail.com>
2021-02-21 17:08:00 -0700

    Rough layout for login screen sections

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Login() {
-  return <p>Hello from Login</p>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen p-10">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with Instagram app"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="flex justify-center w-full">
+          <img
+            src="/images/logo.png"
+            alt="Instagram"
+            className="mt-2 w-6/12 mb-4"
+          />
+        </h1>
+      </div>
+    </div>
+  );
 }
