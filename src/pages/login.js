d50fde3ce195ecee9662508d31362a9ff2f8828e
Katie Williams <kawilliams8@gmail.com>
2021-02-21 17:15:05 -0700

    Add rough login form - no validation or conditional styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,6 +17,26 @@ export default function Login() {
             className="mt-2 w-6/12 mb-4"
           />
         </h1>
+        <form method="POST">
+          <input
+            aria-label="Enter your email address"
+            className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+            type="text"
+            placeholder="Email address"
+          />
+          <input
+            aria-label="Enter your password"
+            className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+            type="password"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+          >
+            Log In
+          </button>
+        </form>
       </div>
     </div>
   );
