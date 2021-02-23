03f486dd1e983acb6b204cc7724dde7732483d70
Katie Williams <kawilliams8@gmail.com>
2021-02-22 20:46:21 -0700

    Add imports to Signup, set up useState for inputs

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import FirebaseContext from "../context/firebase";
+import "@babel/polyfill";
 
 export default function Signup() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   useEffect(() => {
     document.title = "Instagram - Sign Up";
   }, []);
