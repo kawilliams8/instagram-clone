dc6cfe9e7b71a9f6a73ecad065f2b92befea67ab
Katie Williams <kawilliams8@gmail.com>
2021-03-07 13:42:46 -0700

    Add h1/logo to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import * as ROUTES from "../constants/routes";
+import FirebaseContext from "../context/firebase";
 
 export default function Header() {
-  useEffect(() => {
-
-  }, []);
+  const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
 
   return (
-  <p>I am the header</p>
-  )
+    <header className="h-16 bg-white border-b mb-8">
+      <div className="container mx-auto max-width-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1>
+              <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                <img
+                  src="/images/logo.png"
+                  alt="Instagram"
+                  className="mt-2 w-6/12"
+                />
+              </Link>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
