7951fa714585767ecc593fe7f06bbe595082f779
Katie Williams <kawilliams8@gmail.com>
2021-03-07 14:11:17 -0700

    Add mock user and start Header nav buttons w/avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ import FirebaseContext from "../context/firebase";
 export default function Header() {
   const { firebase } = useContext(FirebaseContext);
   const history = useHistory();
+  const user = {
+    displayName: 'raphael'
+  };
 
   return (
     <header className="h-16 bg-white border-b mb-8">
@@ -21,6 +24,60 @@ export default function Header() {
                 />
               </Link>
             </h1>
+          </div>
+          <div className="text-gray text-center flex items-center align-items">
+            {/* Render the header buttons */}
+            {user ? (
+              <>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button className="bg-blue-600 font-bold text-sm rounded text-white w-20 h-8">
+                    temp
+                  </button>
+                </Link>
+                <Link to={ROUTES.LOGIN}>
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue w-20 h-8"
+                    onClick={() => firebase.auth().signOut()}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        firebase.auth().signOut();
+                      }
+                    }}
+                  >
+                    Sign Out
+                  </button>
+                </Link>
+                <div className="flex bg-red-900 items-center cursor-pointer">
+                  <Link to={`/p/${user.displayName}`}>
+                    <img
+                      className="rounded-full h-8 w-8 flex"
+                      src={`/images/avatars/${user.displayName}.jpg`}
+                      alt={`${user.displayName} profile picture`}
+                    />
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <Link to={ROUTES.LOGIN}>
+                  <button
+                    type="button"
+                    className="bg-blue-600 font-bold text-sm rounded text-white w-20 h-8"
+                  >
+                    Log In
+                  </button>
+                </Link>
+                <Link to={ROUTES.SIGN_UP}>
+                  <button
+                    type="button"
+                    className="font-bold text-sm rounded text-blue w-20 h-8"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
