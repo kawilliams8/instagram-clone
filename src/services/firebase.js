3d08d80f55261685620f4552c8e32c3b9c910ce4
Katie Williams <kawilliams8@gmail.com>
2021-03-14 11:56:42 -0600

    Add new firebase service function to getUserByUserId

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,18 @@ export async function doesUsernameExist(username) {
 
   return result.docs.map((user) => user.data().length > 0);
 }
+
+export async function getUserByUserId(id) {
+  const result = await firebase
+    .firestore()
+    .collection("users")
+    .where("userId", "==", id)
+    .get();
+  
+  const user = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return user;
+}
