f808e476c30b418358867c3f413ce06ad9c16fae
Katie Williams <kawilliams8@gmail.com>
2021-04-11 10:14:23 -0600

    Display the username/caption in the Footer component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -8,7 +8,12 @@ import Image from "./image";
 export default function Post({ content }) {
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
-      <Image key={content.docId} src={content.imageSrc} caption={content.caption}/>
+      <Image
+        key={content.docId}
+        src={content.imageSrc}
+        caption={content.caption}
+      />
+      <Footer username={content.username} caption={content.caption} />
     </div>
   );
 }
