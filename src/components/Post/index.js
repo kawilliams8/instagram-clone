cd693c83911fa7542f2c1145f9defcc9d86b925c
Katie Williams <kawilliams8@gmail.com>
2021-04-11 11:23:18 -0600

    Begin Actions component with photo likes

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Actions from "./actions";
 import AddComment from "./add-comment";
 import Footer from "./footer";
@@ -6,12 +6,21 @@ import Header from "./header";
 import Image from "./image";
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+  
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
       <Image
         key={content.docId}
         src={content.imageSrc}
         caption={content.caption}
+      />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
       />
       <Footer username={content.username} caption={content.caption} />
     </div>
