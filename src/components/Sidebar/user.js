1822754304941789ffb25d3caa8f629e572676d3
Katie Williams <kawilliams8@gmail.com>
2021-04-17 10:06:16 -0600

    Add user avatar <Link/> to sidebar

--- src/components/Sidebar/user.js
+++ src/components/Sidebar/user.js
@@ -4,17 +4,24 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 const User = ({ username, fullName}) => {
 
-  return (
-  !username || !fullName ? (
+  return !username || !fullName ? (
     <>
       <Skeleton count={1} height={61} />
     </>
   ) : (
-    <Link to={`/p/${username}`} className="grid -grid-cols-4 gap-4 mb-4 items-center">
-      <p>{username}</p>
+    <Link
+      to={`/p/${username}`}
+      className="grid -grid-cols-4 gap-4 mb-4 items-center"
+    >
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full h-16 w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt="My Profile"
+        />
+      </div>
     </Link>
-  )
-  )
+  );
 }
 
 export default User;
