17b3328c2a5983dbac38a44b24a787eb4d3867d4
Katie Williams <kawilliams8@gmail.com>
2021-04-17 10:10:12 -0600

    Display the username/fullName in Sidebar

--- src/components/Sidebar/user.js
+++ src/components/Sidebar/user.js
@@ -11,14 +11,18 @@ const User = ({ username, fullName}) => {
   ) : (
     <Link
       to={`/p/${username}`}
-      className="grid -grid-cols-4 gap-4 mb-4 items-center"
+      className="grid grid-cols-4 gap-4 mb-4 items-center"
     >
       <div className="flex items-center justify-between col-span-1">
         <img
-          className="rounded-full h-16 w-16 flex mr-3"
+          className="rounded-full w-16 flex mr-3"
           src={`/images/avatars/${username}.jpg`}
           alt="My Profile"
         />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
       </div>
     </Link>
   );
