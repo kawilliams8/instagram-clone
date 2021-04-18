90cdeac6b135aecf7cb03d885dc082730cf3025f
Katie Williams <kawilliams8@gmail.com>
2021-04-18 11:30:06 -0600

    Set up useReducer and useEffect to begin storing state on a profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import ProfileHeader from './profileHeader';
 import ProfilePhotos from './profilePhotos';
+import { getUserByUsername } from '../../services/firebase'
 
 export default function UserProfile({username}) {
+  const reducer = (state, newState) => ({...state, ...newState});
+  const initialState = {
+    profile: {},
+    photos: [],
+    followerCount: 0,
+  }
+
+  const [{ profile, photos, followerCount }, dispatch] = useReducer(
+    reducer,
+    initialState
+  )
+
+  useEffect(() => {
+    async function getProfileAndPhotos() {
+      const [{ ...user } ] = await getUserByUsername(username);
+
+    }
+    getProfileAndPhotos();
+  }, [username])
 
   return (
     <div>
