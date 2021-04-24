19ac6072deb016067b8c1fbcf185e5ccda5d148f
Katie Williams <kawilliams8@gmail.com>
2021-04-24 17:56:33 -0600

    Add more profile metadata to header: fullname, counts

--- src/components/Profile/profileHeader.js
+++ src/components/Profile/profileHeader.js
@@ -1,12 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
+import useUser from "../../hooks/use-user";
+import Skeleton from "react-loading-skeleton";
 
 export default function ProfileHeader({
   photosCount,
   followerCount,
   setFollowerCount,
-  profile,
   username,
+  profile
 }) {
+  const [isFollowingProfile, setIsFollowingProfile] = useState(false);
+  const { user } = useUser();
+  const activeFollowBtn = user.username && user.username !== username;
+  
   return (
     <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
       <div className="container flex justify-center">
@@ -19,6 +25,42 @@ export default function ProfileHeader({
       <div className="flex items-center justify-center flex-col col-span-2">
         <div className="container flex items-center">
           <p className="text-2xl mr-4">{username}</p>
+          {activeFollowBtn && (
+            <button
+              className="bg-blue-500 font-bold text-sm rounded text-white w-20 h-8"
+              type="button"
+              onClick={() => console.log("I am a button")}
+            >
+              {true ? "Unfollow" : "Follow"}
+            </button>
+          )}
+        </div>
+        <div className="container flex mt-4">
+          {profile.followers === undefined ||
+          profile.following === undefined ? (
+            <Skeleton count={1} width={677} height={24} />
+          ) : (
+            <>
+              <p className="mr-10">
+                <span className="font-bold">{photosCount}</span> photos
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">{followerCount}</span>{" "}
+                {followerCount === 1 ? "follower" : "followers"}
+              </p>
+              <p className="mr-10">
+                <span className="font-bold">
+                  {profile.following.length || 0}
+                </span>{" "}
+                following
+              </p>
+            </>
+          )}
+        </div>
+        <div className="container mt-4">
+          <p className="font-medium">
+            {!profile.fullName ? <Skeleton count={1} height={24} /> : profile.fullName}
+          </p>
         </div>
       </div>
     </div>
