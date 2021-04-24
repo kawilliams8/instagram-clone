3f55f2cd1ee22b69ae3c24d8b3a4856ea0a388cf
Katie Williams <kawilliams8@gmail.com>
2021-04-24 16:50:17 -0600

    Set up three conditions for profile photos: Loading Skelton, none, or map over and display

--- src/components/Profile/profilePhotos.js
+++ src/components/Profile/profilePhotos.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Skeleton from "react-loading-skeleton";
 
 export default function ProfilePhotos({ photos }) {
-  return photos.length === 0 ? (
-    <p>No photos to display yet.</p>
-  ) : (
-    <>
-      <Skeleton count={9} height={320} width={400} />
-    </>
-  );
+  return (
+    <div className="h-16 border-t border-gray mt-12 pt-4">
+      { !photos ? (
+          <Skeleton count={9} height={320} width={400} />
+        ) : photos.length > 0 ? (
+          photos.map((photo, index) => {
+            return <img key={index} src={photo.imageSrc}/>
+          })
+        ) : (
+          <p>No photos to display yet.</p>
+        )
+      }
+    </div>
+  )
 }
