f6269f2551b0d9862fc0eb9d68c32f59e837ab9f
Katie Williams <kawilliams8@gmail.com>
2021-04-24 16:55:05 -0600

    Add 3x3 grid to displayed photos

--- src/components/Profile/profilePhotos.js
+++ src/components/Profile/profilePhotos.js
@@ -4,16 +4,21 @@ import Skeleton from "react-loading-skeleton";
 export default function ProfilePhotos({ photos }) {
   return (
     <div className="h-16 border-t border-gray mt-12 pt-4">
-      { !photos ? (
+      <div className="grid grid-cols-3 gap-8 mt-4 mb-12">
+        {!photos ? (
           <Skeleton count={9} height={320} width={400} />
         ) : photos.length > 0 ? (
           photos.map((photo, index) => {
-            return <img key={index} src={photo.imageSrc}/>
+            return (
+              <div key={photo.docId} className="relative group">
+                <img key={index} src={photo.imageSrc} alt={photo.caption} />
+              </div>
+            );
           })
         ) : (
           <p>No photos to display yet.</p>
-        )
-      }
+        )}
+      </div>
     </div>
-  )
+  );
 }
