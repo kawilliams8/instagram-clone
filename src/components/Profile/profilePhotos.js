8324627ee53943a6f9cdabd4037057aa94c0ad47
Katie Williams <kawilliams8@gmail.com>
2021-04-24 17:09:05 -0600

    Add heart SVG and likes count to each photo

--- src/components/Profile/profilePhotos.js
+++ src/components/Profile/profilePhotos.js
@@ -12,6 +12,37 @@ export default function ProfilePhotos({ photos }) {
             return (
               <div key={photo.docId} className="relative group">
                 <img key={index} src={photo.imageSrc} alt={photo.caption} />
+                <div className="flex flex-start ml-1">
+                  <svg
+                    // onClick={() =>
+                    //   handleToggleLiked((toggleLiked) => !toggleLiked)
+                    // }
+                    // onKeyDown={(e) => {
+                    //   if (e.key === "Enter") {
+                    //     handleToggleLiked((toggleLiked) => !toggleLiked);
+                    //   }
+                    // }}
+                    key={'svg' + index}
+                    className={`w-8 mr-2 select-none cursor-pointer fill-current stroke-current text-red-500`}
+                    // ${
+                    // toggleLiked
+                    //   ? "fill-current stroke-current text-red-500"
+                    //   : "text-black"
+                    // }
+                    // `}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    tabIndex={0}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
+                  <div className="text-lg mt-1">{photo.likes.length}</div>
+                </div>
               </div>
             );
           })
